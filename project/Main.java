9d3492a0ba0ec2f2cb8e5a957b767f699bd21493
Ej Sobrepena <ej.sobrepena@tuni.fi>
2024-03-18 21:45:19 +0200

    project/Main.java: Initial commit with Main.java as the application entry point.

---
+++ project/Main.java
@@ -0,0 +1,20 @@
+/**
+ * @author      Ej Sobrepena <ej.sobrepena@tuni.fi>
+ * @version     2024.03.18 (last modified)
+ * @since       17.0 (minimum Java version)
+ */
+public class Main {
+    /**
+     * Starts the application.
+     *
+     * <p>Prints a message indicating the application has started and
+     * instructs the user to navigate to other classes for functionality.
+     *
+     * @param args Command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+        System.out.println("Application started. Please navigate to other classes to see functionality.");
+    }
+}
+
+// End of file
