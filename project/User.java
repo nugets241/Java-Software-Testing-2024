4c6f60b040bcef8e1c4668e6c31995cc12983717
Ej Sobrepena <ej.sobrepena@tuni.fi>
2024-03-19 11:13:07 +0200

    project/User: Add User class with basic functionality

---
+++ project/User.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author      Ej Sobrepena <ej.sobrepena@tuni.fi>
+ * @version     2024.03.19 (last modified)
+ * @since       17.0 (minimum Java version)
+ */
+public class User {
+    private String name;
+    private String email;
+    private List<Book> borrowedBooks;
+
+    /**
+     * Creates a new user.
+     *
+     * @param name The name of the user.
+     * @param email The email of the user.
+     */
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    /**
+     * Returns the name of the user.
+     *
+     * @return The name of the user.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the email of the user.
+     *
+     * @return The email of the user.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Allows the user to borrow a book.
+     *
+     * @param book The book to be borrowed.
+     */
+    public void borrowBook(Book book) {
+        if (book.isAvailable()) {
+            borrowedBooks.add(book);
+            book.setAvailable(false);
+        }
+    }
+
+    /**
+     * Returns the books borrowed by the user.
+     *
+     * @return The books borrowed by the user.
+     */
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    /**
+     * Allows the user to return a book.
+     *
+     * @param book The book to be returned.
+     */
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+        book.setAvailable(true);
+    }
+}
