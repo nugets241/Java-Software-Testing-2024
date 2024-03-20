47d4693e7bbdc7a7a2acc56869bf55572f0d9875
sobrepenaej@yahoo.com <sobrepenaej@yahoo.com>
2024-03-20 12:29:48 +0200

    basic/032: Add homework

---
+++ basic/032/LoopSeries.java
@@ -0,0 +1,36 @@
+/**
+ * @author      Ej Sobrepena <ej.sobrepena@tuni.fi>
+ * @version     2024.03.20 (last modified)
+ * @since       17.0 (minimum Java version)
+ */
+public class LoopSeries {
+
+    /**
+     * Prints a series of 10-based column numbers up to the given length.
+     * The last number is substituted with a space.
+     *
+     * @param length  The length of the series to print.
+     */
+    public static void printSeries(int length) {
+        for (int i = 1; i <= length; i++) {
+            if (i % 10 == 0) {
+                System.out.print(" ");
+            } else {
+                System.out.print(i % 10);
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * Implements exercise 3.2.
+     *
+     * Demonstrates the printSeries method.
+     *
+     * @param args Command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+        int length = 25;
+        printSeries(length);
+    }
+}
