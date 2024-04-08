c881b1c686f3a44e2b954721c0a0a6d71d424795
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2024-04-09 02:37:21 +0300

    project/Transaction: Add input validation to Transaction class

--- project/Transaction.java
+++ project/Transaction.java
@@ -1,3 +1,4 @@
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -23,6 +24,11 @@ public class Transaction {
     private Date borrowDate;
 
     /**
+     * The date by which the book should be returned.
+     */
+    private Date dueDate;
+
+    /**
      * The return date of the transaction.
      */
     private Date returnDate;
@@ -32,11 +38,18 @@ public class Transaction {
      *
      * @param user The user who borrows the book.
      * @param book The book that is borrowed.
+     * @throws IllegalArgumentException If the user or book is null.
      */
     public Transaction(User user, Book book) {
+        if (user == null) {
+            throw new IllegalArgumentException("User must not be null");
+        } else if (book == null) {
+            throw new IllegalArgumentException("Book must not be null");
+        }
         this.user = user;
         this.book = book;
         this.borrowDate = new Date();
+        this.dueDate = new Date(14 * 24 * 60 * 60 * 1000L + borrowDate.getTime());
         this.returnDate = null;
     }
 
@@ -46,6 +59,9 @@ public class Transaction {
      * @return The user of the transaction.
      */
     public User getUser() {
+        if (user == null) {
+            throw new IllegalArgumentException("User must not be null");
+        }
         return user;
     }
 
@@ -59,15 +75,6 @@ public class Transaction {
     }
 
     /**
-     * Returns the borrow date of the transaction.
-     *
-     * @return The borrow date of the transaction.
-     */
-    public Date getBorrowDate() {
-        return borrowDate;
-    }
-
-    /**
      * Returns the return date of the transaction.
      *
      * @return The return date of the transaction.
@@ -77,11 +84,27 @@ public class Transaction {
     }
 
     /**
+     * Returns the borrow date of the transaction.
+     *
+     * @return The borrow date of the transaction.
+     */
+    public Date getBorrowDate() {
+        return borrowDate;
+    }
+
+    /**
      * Sets the return date of the transaction.
      *
      * @param returnDate The return date of the transaction.
+     * @throws IllegalArgumentException If the return date is null or before
+     *                                  the borrow date.
      */
     public void setReturnDate(Date returnDate) {
+        if (returnDate == null) {
+            throw new IllegalArgumentException("Return date must not be null");
+        } else if (returnDate.before(borrowDate)) {
+            throw new IllegalArgumentException("Return date must not be before borrow date");
+        }
         this.returnDate = returnDate;
     }
 
@@ -92,5 +115,30 @@ public class Transaction {
      */
     public boolean isReturned() {
         return returnDate != null;
+    }
+
+    /**
+     * Sets the due date of the transaction.
+     *
+     * @param days The number of days to borrow the book.
+     * @throws IllegalArgumentException If the days is less than or equal to 0.
+     */
+    public void extendDueDate(int extensionDays) {
+        if (extensionDays <= 0) {
+            throw new IllegalArgumentException("Extension days must be greater than 0");
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(this.returnDate);
+        calendar.add(Calendar.DATE, extensionDays);
+        this.returnDate = calendar.getTime();
+    }
+
+    /**
+     * Returns the due date of the transaction.
+     *
+     * @return The due date of the transaction.
+     */
+    public Date getDueDate() {
+        return dueDate;
     }
 }
